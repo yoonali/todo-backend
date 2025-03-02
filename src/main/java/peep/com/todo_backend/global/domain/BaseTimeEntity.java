245fad30d916754c4903ff0f3b0c30d0243cc979
yoona <103414935+yoonali@users.noreply.github.com>
2025-03-02 15:36:28 +0900

    fix: #@Getter 추가

--- src/main/java/peep/com/todo_backend/global/domain/BaseTimeEntity.java
+++ src/main/java/peep/com/todo_backend/global/domain/BaseTimeEntity.java
@@ -3,6 +3,8 @@ package peep.com.todo_backend.global.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 import org.hibernate.annotations.Comment;
 import org.springframework.data.annotation.CreatedDate;
@@ -11,6 +13,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseTimeEntity {
     @Comment("생성 시간")
     @Column(nullable = false, name = "created_date")
