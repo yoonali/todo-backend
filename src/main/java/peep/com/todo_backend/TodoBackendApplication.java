76f4d2b3511594fd48ef0b841f4d922faf7c214d
yoona <103414935+yoonali@users.noreply.github.com>
2025-03-02 15:28:53 +0900

    feat: #Jpa Auditing 활성화
    BaseTimeEntity에서 @CreatedDate와 @LastModifiedDate를 사용하기 위해 JPA Auditing을 활성화해야 합니다. Spring Boot 메인 애플리케이션 클래스에 @EnableJpaAuditing 어노테이션을 추가

--- src/main/java/peep/com/todo_backend/TodoBackendApplication.java
+++ src/main/java/peep/com/todo_backend/TodoBackendApplication.java
@@ -2,8 +2,10 @@ package peep.com.todo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TodoBackendApplication {
 
 	public static void main(String[] args) {
