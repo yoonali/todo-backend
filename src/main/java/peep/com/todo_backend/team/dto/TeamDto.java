1d97e82a4d95b1eee39076b847ece55de1e415f1
yoona <103414935+yoonali@users.noreply.github.com>
2025-03-20 13:42:44 +0900

    refactor: #DTO example표시

--- src/main/java/peep/com/todo_backend/team/dto/TeamDto.java
+++ src/main/java/peep/com/todo_backend/team/dto/TeamDto.java
@@ -2,6 +2,7 @@ package peep.com.todo_backend.team.dto;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +11,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class TeamDto {
+
+    @Schema(description = "팀 이름", example = "PEEP")
     protected String name;
+
+    @Schema(description = "프로젝트 이름", example = "TODO")
     protected String projectName;
+
+    @Schema(description = "프로젝트 설명", example = "할 일을 관리하는 프로젝트")
     protected String description;
+
+    @Schema(description = "프로젝트 시작일", example = "2025-03-20")
     protected LocalDate startDate;
+
+    @Schema(description = "프로젝트 종료일", example = "2021-04-05")
     protected LocalDate endDate;
 }
-
-
